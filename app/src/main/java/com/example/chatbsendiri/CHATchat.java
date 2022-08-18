598c2994c095e0902df1ea07f9fe77bdf33368ab
Sofian Layunta <sofianlayunta27@gmail.com>
2022-08-18 16:39:38 +0700

    secondCommit

--- app/src/main/java/com/example/chatbsendiri/CHATchat.java
+++ app/src/main/java/com/example/chatbsendiri/CHATchat.java
@@ -3,12 +3,90 @@ package com.example.chatbsendiri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.emitter.Emitter;
 
 public class CHATchat extends AppCompatActivity {
+
+    public Button button;
+    public TextView text;
+    public EditText edit;
+    public String message;
+    private io.socket.client.Socket mSocket;
+
+    {
+        try {
+            mSocket = IO.socket("http://10.0.112.194:8765");
+            mSocket.connect();
+            Log.d("loglog", "instance initializer: "+mSocket.connected());
+        } catch (URISyntaxException e) {
+            Log.d("myTag", e.getMessage());
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatchat);
+        button = (Button) findViewById(R.id.button);
+        text =  (TextView) findViewById(R.id.textView);
+        edit =  (EditText) findViewById(R.id.editText);
+        mSocket.connect();
+        text.setText("");
+        setListening();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                String message = edit.getText().toString().trim();
+                edit.setText("");
+                if (!message.isEmpty()) {
+                    //send message
+                    String jsonString = "{message: " + "'" + message + "'" + "}";
+
+                    try {
+                        JSONObject jsonData = new JSONObject(jsonString);
+                        mSocket.emit("message", jsonData);
+                    } catch (JSONException e) {
+                        Log.d("me", "error send message " + e.getMessage());
+                    }
+                }
+            }
+        });
+
+    }//on create
+    private void setListening() {
+        mSocket.on("message", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                try {
+                    Log.d("loglog", "call: "+args[0].toString());
+                    JSONObject messageJson = new JSONObject(args[0].toString());
+                    message = messageJson.getString("message");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText(message);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d("loglog", "call: "+e.getMessage());
+                }
+            }
+        });
     }
-}
+
+}//end of class
--- app/src/main/java/com/example/chatbsendiri/ChatBoxAdapter.java
+++ app/src/main/java/com/example/chatbsendiri/ChatBoxAdapter.java
@@ -1,2 +1,92 @@
-package com.example.chatbsendiri;public class ChatBoxAdapter {
+package com.example.chatbsendiri;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatBoxAdapter  extends RecyclerView.Adapter<ChatBoxAdapter.MyViewHolder> {
+    private List<Message> MessageList;
+    private String nickname;
+
+    public  class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView nickname;
+        public TextView message;
+        public LinearLayout llChat;
+
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            nickname = (TextView) view.findViewById(R.id.nickname);
+            message = (TextView) view.findViewById(R.id.message);
+            llChat = (LinearLayout) view.findViewById(R.id.llChat);
+
+
+
+
+
+        }
+    }
+// in this adaper constructor we add the list of messages as a parameter so that
+// we will passe  it when making an instance of the adapter object in our activity
+
+
+
+    public ChatBoxAdapter(List<Message>MessagesList, String nickname) {
+
+        this.MessageList = MessagesList;
+        this.nickname = nickname;
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return MessageList.size();
+    }
+    @Override
+    public ChatBoxAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+
+
+
+        return new ChatBoxAdapter.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final ChatBoxAdapter.MyViewHolder holder, final int position) {
+
+        //binding the data from our ArrayList of object to the item.xml using the viewholder
+        Message m = MessageList.get(position);
+        holder.nickname.setText(m.getNickname());
+        holder.message.setText(m.getMessage() );
+
+        if (m.getNickname().equals(nickname)){
+            holder.llChat.setGravity(Gravity.END);
+            holder.message.setBackgroundResource(R.drawable.round2);
+            holder.nickname.setTextColor(Color.parseColor("#579296"));
+
+        }else {
+            holder.llChat.setGravity(Gravity.START);
+            holder.message.setBackgroundResource(R.drawable.round1);
+            holder.nickname.setTextColor(Color.parseColor("#4a7dcf"));
+        }
+
+
+
+
+    }
+
+
+
 }
